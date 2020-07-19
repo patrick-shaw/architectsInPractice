4bf56cea8eb225300082cbfe877809c2a0e87728
Patrick Shaw <patrickshaw19@hotmail.co.uk>
2020-07-20 00:16:08 +0100

    person collapse

--- js/main.js
+++ js/main.js
@@ -24,6 +24,7 @@ isMobileMenu(x)
 
 window.addEventListener('resize', () => {
     isMobileMenu(x);
+    removeCollapse(x);
 })
 
 
@@ -83,9 +84,31 @@ projectContainer.forEach(item => {
 })
 
 //read more collapsed
-const readMore = document.getElementById('read-more');
-readMore.addEventListener('click', () => {
-    setTimeout(() => {
-        !readMore.classList.contains('collapsed') ? readMore.innerHTML = 'Read less' : readMore.innerHTML = 'Read more'
-    }, 200)
+const readMore = document.querySelectorAll('#read-more');
+readMore.forEach(item => {
+    item.addEventListener('click', () => {
+        const personInfo = item.closest('.person-info-wrap');
+        !personInfo.classList.contains('info-height') ? personInfo.classList.add('info-height') : personInfo.classList.remove('info-height');
+        setTimeout(() => {
+            !personInfo.classList.contains('info-height') ? item.innerHTML = 'Read more' : item.innerHTML = 'Read less'
+        }, 200)
+    })
 })
+
+
+const personCollapse = document.querySelectorAll('#collapse-readmore');
+
+
+
+function removeCollapse(x) {
+    personCollapse.forEach((item) => {
+        if (!x.matches) {
+            item.classList.remove('collapse');
+        } else {
+            item.classList.add('collapse');
+        }
+    })
+}
+
+removeCollapse(x);
+
